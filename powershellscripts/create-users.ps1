2608cf47c07e704001cc909f99efd53ad9752fa6
VladislavsK5 <kurss.vladislavs@gmail.com>
2026-07-09 11:33:37 +0300

    Add script to create users from CSV in AD
    This script imports users from a CSV file and creates Active Directory accounts for them.

--- powershellscripts/create-users.ps1
+++ powershellscripts/create-users.ps1
@@ -1,1 +1,16 @@
+Import-Module ActiveDirectory
 
+$users = Import-Csv ".\users.csv"
+
+foreach ($user in $users) {
+
+    $password = ConvertTo-SecureString $user.Password -AsPlainText -Force
+
+    New-ADUser `
+        -Name "$($user.FirstName) $($user.LastName)" `
+        -SamAccountName $user.Username `
+        -AccountPassword $password `
+        -Enabled $true `
+        -Path "OU=$($user.Department),OU=Users,DC=homelab,DC=local"
+
+}
